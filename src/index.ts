7bd78e8fba567cec0227af66ee5fa459c1e28845
SicdeX <sicdex@gmail.com>
2024-08-27 04:08:34 +0600

    fix: new logic

--- src/index.ts
+++ src/index.ts
@@ -17,18 +17,15 @@ import {
 	NetworkedParticle,
 	PhysicalItem,
 	PlayerCustomData,
-	TickSleeper
+	TaskManager
 } from "github.com/octarine-public/wrapper/index"
 
 new (class CAbuseMidas {
-	private isSelled = false
-	private isPurchased = false
-
 	private pickItem: Nullable<PhysicalItem>
 	private droppedItem: Nullable<item_hand_of_midas>
-
 	private midasName = "item_hand_of_midas"
-	private readonly sleeper = new TickSleeper()
+	private task: Nullable<bigint>
+
 	private readonly iconNode = ImageData.GetItemTexture(this.midasName)
 	private toolTip =
 		"!!! Use own risk and lucky !!!\nFor abuse need Hand Of Midas and 2200 gold"
@@ -48,14 +45,13 @@ new (class CAbuseMidas {
 		EventsSDK.on("EntityCreated", this.EntityCreated.bind(this))
 		EventsSDK.on("ParticleCreated", this.ParticleCreated.bind(this))
 		EventsSDK.on("EntityDestroyed", this.EntityDestroyed.bind(this))
-		EventsSDK.on("PostDataUpdate", this.PostDataUpdate.bind(this))
 		EventsSDK.on("PrepareUnitOrders", this.PrepareUnitOrders.bind(this))
 		EventsSDK.on("LifeStateChanged", this.LifeStateChanged.bind(this))
 		this.state.OnDeactivate(() => this.reset())
 	}
 
 	private get delay() {
-		return (GameState.InputLag + GameState.IOLag) * 1000
+		return GameState.InputLag * 1000
 	}
 
 	protected GameEnded() {
@@ -67,6 +63,9 @@ new (class CAbuseMidas {
 	}
 
 	protected ChatEvent(type: DOTA_CHAT_MESSAGE, value: number, ...args: number[]) {
+		if (!this.state.value) {
+			return
+		}
 		if (type !== DOTA_CHAT_MESSAGE.CHAT_MESSAGE_ITEM_PURCHASE || value !== 65) {
 			return
 		}
@@ -86,45 +85,21 @@ new (class CAbuseMidas {
 		if (!this.hasGold(playerTeamData)) {
 			return
 		}
-
-		this.isPurchased = true
-
-		const stashMidas = localHero.TotalItems.find(
-			item => item instanceof item_hand_of_midas
-		)
-		if (
-			stashMidas !== undefined &&
-			stashMidas.ItemSlot >= DOTAScriptInventorySlot.DOTA_STASH_SLOT_1 &&
-			stashMidas.ItemSlot <= DOTAScriptInventorySlot.DOTA_STASH_SLOT_6
-		) {
-			localHero.SellItem(stashMidas)
-			this.sleeper.Sleep(this.delay)
-			this.isSelled = true
-		}
-	}
-
-	protected PostDataUpdate(dt: number) {
-		if (dt === 0 || !this.state.value || this.sleeper.Sleeping) {
-			return
-		}
-		const localData = this.customData()
-		if (localData === undefined || this.pickItem === undefined) {
-			return
-		}
-		const [playerData, localHero] = localData
-		if (!this.hasGold(playerData)) {
-			return
-		}
-		if (!this.isPurchased) {
-			localHero.PurchaseItem(25)
-			localHero.PurchaseItem(64)
-			this.sleeper.Sleep(this.delay)
-			return
-		}
-		if (this.isSelled) {
-			localHero.PickupItem(this.pickItem)
-			this.isSelled = false
-		}
+		this.task = TaskManager.Begin(() => {
+			const stashMidas = localHero.TotalItems.find(
+				item => item instanceof item_hand_of_midas
+			)
+			if (
+				stashMidas !== undefined &&
+				stashMidas.ItemSlot >= DOTAScriptInventorySlot.DOTA_STASH_SLOT_1 &&
+				stashMidas.ItemSlot <= DOTAScriptInventorySlot.DOTA_STASH_SLOT_6
+			) {
+				localHero.SellItem(stashMidas)
+			}
+			if (this.pickItem !== undefined) {
+				localHero.PickupItem(this.pickItem)
+			}
+		}, this.delay)
 	}
 
 	protected PrepareUnitOrders(order: ExecuteOrder): void | false {
@@ -174,12 +149,14 @@ new (class CAbuseMidas {
 	protected EntityDestroyed(entity: Entity) {
 		if (this.pickItem === entity) {
 			this.pickItem = undefined
-			this.isPurchased = false
 		}
 	}
 
 	protected LifeStateChanged(entity: Entity) {
 		if (entity === LocalPlayer?.Hero && !entity.IsAlive) {
+			if (this.task !== undefined) {
+				TaskManager.Cancel(this.task)
+			}
 			this.reset()
 		}
 	}
@@ -204,6 +181,8 @@ new (class CAbuseMidas {
 			return
 		}
 		if (this.droppedItem !== undefined) {
+			localHero.PurchaseItem(25)
+			localHero.PurchaseItem(64)
 			this.dropMidas(this.droppedItem)
 		}
 	}
@@ -214,7 +193,7 @@ new (class CAbuseMidas {
 		}
 		const owner = midas.Owner
 		if (owner !== undefined) {
-			owner.DropItem(midas, owner.Position)
+			owner.DropItem(midas, owner.InFront(owner.HullRadius))
 		}
 	}
 
@@ -239,11 +218,9 @@ new (class CAbuseMidas {
 	}
 
 	private reset() {
-		this.isSelled = false
-		this.isPurchased = false
+		this.task = undefined
 		this.pickItem = undefined
 		this.droppedItem = undefined
-		this.sleeper.ResetTimer()
 	}
 
 	private hasGold(playerData: PlayerCustomData) {
