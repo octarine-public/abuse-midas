a19dc74308eb58ca28048cbf7cdedec438453837
Nikita <donatedimas@gmail.com>
2024-08-27 08:43:10 +0300

    use courier fix

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import {
 	DOTAScriptInventorySlot,
 	dotaunitorder_t,
 	Entity,
+	EntityManager,
 	EventsSDK,
 	ExecuteOrder,
 	GameState,
@@ -15,20 +16,19 @@ import {
 	LocalPlayer,
 	Menu,
 	NetworkedParticle,
-	PhysicalItem,
 	PlayerCustomData,
-	TaskManager
+	TaskManager,
+	Unit
 } from "github.com/octarine-public/wrapper/index"
 
 new (class CAbuseMidas {
-	private pickItem: Nullable<PhysicalItem>
 	private droppedItem: Nullable<item_hand_of_midas>
 	private midasName = "item_hand_of_midas"
 	private task: Nullable<bigint>
 
 	private readonly iconNode = ImageData.GetItemTexture(this.midasName)
 	private toolTip =
-		"!!! Use own risk and lucky !!!\nFor abuse need Hand Of Midas and 2200 gold"
+		"!!! Use own risk and lucky !!!\nKeep courier close to you.\nFor abuse need Hand Of Midas and 2200 gold. "
 
 	private readonly entry = Menu.AddEntry("Utility")
 	private readonly menu = this.entry.AddNode(
@@ -42,9 +42,7 @@ new (class CAbuseMidas {
 		EventsSDK.on("ChatEvent", this.ChatEvent.bind(this))
 		EventsSDK.on("GameEnded", this.GameEnded.bind(this))
 		EventsSDK.on("GameStarted", this.GameStarted.bind(this))
-		EventsSDK.on("EntityCreated", this.EntityCreated.bind(this))
 		EventsSDK.on("ParticleCreated", this.ParticleCreated.bind(this))
-		EventsSDK.on("EntityDestroyed", this.EntityDestroyed.bind(this))
 		EventsSDK.on("PrepareUnitOrders", this.PrepareUnitOrders.bind(this))
 		EventsSDK.on("LifeStateChanged", this.LifeStateChanged.bind(this))
 		this.state.OnDeactivate(() => this.reset())
@@ -90,42 +88,12 @@ new (class CAbuseMidas {
 		if (localData[1].IsVisibleForEnemies() && localData[1].HPPercent < 3) {
 			return
 		}
-		if (this.pickItem !== undefined) {
-			return false
-		}
 		if (order.OrderType !== dotaunitorder_t.DOTA_UNIT_ORDER_CAST_TARGET) {
 			return
 		}
 		const ability = order.Ability_
 		if (ability instanceof item_hand_of_midas && ability.StackCount <= 0) {
 			this.droppedItem = ability
-		}
-	}
-
-	protected EntityCreated(entity: Entity) {
-		if (!(entity instanceof PhysicalItem)) {
-			return
-		}
-		const midas = entity.Item
-		if (!(midas instanceof item_hand_of_midas)) {
-			return
-		}
-		if (this.droppedItem !== midas) {
-			return
-		}
-		const pLocalData = this.customData()
-		if (pLocalData === undefined) {
-			return
-		}
-		if (midas.Owner === pLocalData[1]) {
-			this.pickItem = entity
-			this.droppedItem = undefined
-		}
-	}
-
-	protected EntityDestroyed(entity: Entity) {
-		if (this.pickItem === entity) {
-			this.pickItem = undefined
 		}
 	}
 
@@ -155,19 +123,48 @@ new (class CAbuseMidas {
 			return
 		}
 		if (this.droppedItem !== undefined && this.droppedItem.StackCount <= 1) {
-			localHero.PurchaseItem(25)
-			localHero.PurchaseItem(64)
-			this.dropMidas(this.droppedItem)
+			const courier = this.getCourier()
+			if (courier !== undefined) {
+				if (courier.Position.Distance(localHero.Position) < 200) {
+					localHero.PurchaseItem(25)
+					localHero.PurchaseItem(64)
+					this.dropMidas(this.droppedItem)
+				}
+			}
 		}
 	}
-
+	private getCourier(): Nullable<Unit> {
+		return EntityManager.AllEntities.filter(
+			ent =>
+				ent.RootOwner === LocalPlayer &&
+				ent.IsAlive &&
+				ent instanceof Unit &&
+				ent.IsCourier
+		).at(0) as Nullable<Unit>
+	}
+	private followLocalHero(courier: Unit) {
+		const localHero = this.customData()?.[1]
+		if (localHero !== undefined) {
+			ExecuteOrder.PrepareOrder({
+				orderType: dotaunitorder_t.DOTA_UNIT_ORDER_MOVE_TO_TARGET,
+				issuers: [courier],
+				target: localHero,
+				queue: true,
+				isPlayerInput: false
+			})
+		}
+	}
 	private dropMidas(midas: item_hand_of_midas) {
 		if (!this.state.value) {
 			return
 		}
 		const owner = midas.Owner
+
 		if (owner !== undefined) {
-			owner.DropItem(midas, owner.InFront(owner.HullRadius))
+			const courier = this.getCourier()
+			if (courier !== undefined && courier !== owner) {
+				owner.GiveItem(midas, courier)
+			}
 		}
 	}
 
@@ -181,6 +178,7 @@ new (class CAbuseMidas {
 		if (playerTeamData === undefined) {
 			return
 		}
+
 		const localHero = playerTeamData.Hero
 		if (localHero === undefined || !localHero.IsAlive) {
 			return
@@ -196,7 +194,6 @@ new (class CAbuseMidas {
 			TaskManager.Cancel(this.task)
 		}
 		this.task = undefined
-		this.pickItem = undefined
 		this.droppedItem = undefined
 	}
 
@@ -212,20 +209,27 @@ new (class CAbuseMidas {
 			this.reset()
 			return
 		}
+
 		const [, localHero] = localData
-		if (this.pickItem === undefined) {
+		if (this.droppedItem === undefined) {
 			return
 		}
+
 		const stashMidas = localHero.TotalItems.find(
-			item => item instanceof item_hand_of_midas
+			item =>
+				item instanceof item_hand_of_midas &&
+				item.ItemSlot >= DOTAScriptInventorySlot.DOTA_STASH_SLOT_1 &&
+				item.ItemSlot <= DOTAScriptInventorySlot.DOTA_STASH_SLOT_6
 		)
-		if (
-			stashMidas !== undefined &&
-			stashMidas.ItemSlot >= DOTAScriptInventorySlot.DOTA_STASH_SLOT_1 &&
-			stashMidas.ItemSlot <= DOTAScriptInventorySlot.DOTA_STASH_SLOT_6
-		) {
+		if (stashMidas !== undefined) {
 			localHero.SellItem(stashMidas)
 		}
-		localHero.PickupItem(this.pickItem)
+
+		const courier = this.getCourier()
+		if (courier !== undefined && courier === this.droppedItem.Owner) {
+			const item = this.droppedItem
+			courier.GiveItem(item, localHero, true)
+			this.followLocalHero(courier)
+		}
 	}
 })()
